ac2e93d3378cff13fc5266c7020a3854ca9363b9
maba99 <courses649@gmail.com>
2024-10-03 16:08:29 +0400

    Update PasswordEncoderTest.java

--- servicedesc2/src/main/java/com/service_desc_2/servicedesc2/config/PasswordEncoderTest.java
+++ servicedesc2/src/main/java/com/service_desc_2/servicedesc2/config/PasswordEncoderTest.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordEncoderTest {
     public static void main(String[] args) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodedPassword = passwordEncoder.encode("superadmin"); // Для супер администратора
+        String encodedPassword = passwordEncoder.encode("superadmin"); // For superadmin
         System.out.println(encodedPassword);
     }
 }
